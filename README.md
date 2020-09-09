cf729b97a620182b8e2d3fc97861e3985da9562b
najimali <najim.ali32@gmail.com>
2020-09-09 21:37:08 +0530

    Home Page is Responsive now

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # task 1
 Design Home page
-    1. Desing Nav bar
-    2. Design Main Container
+1. Desing Nav bar
+2. Design Main Container
+# task 2 
+1. Setup backend
+2. create new form & feed them to home page
     
--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const express =require('express'),
 app.use(express.static(__dirname+"/public"));
 app.set("view engine","ejs");
 
+app.get("/",(req,res)=>{
 
+    res.render("index");
+});
 
 app.listen(3000,()=>{
     console.log("Blog Site Server started");
